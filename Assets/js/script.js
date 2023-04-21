40c960efb43146b469019384bf1adc9562478be0
criatura435 <127697449+criatura435@users.noreply.github.com>
2023-04-21 15:38:16 -0500

    Initial files & psuedocode

---
+++ Assets/js/script.js
@@ -0,0 +1,9 @@
+// on start button click:
+// while questions !last && timer !zero
+// display question w/ 4 answer options
+// if answer is correct: add 1 to score; display message; display next question
+// else display message; display next question; deduct extra time
+// once questions == last || timer == zero: display form to add initials w/ score
+// display initials with scores ranked
+// offer repeat - scores must persist
+
