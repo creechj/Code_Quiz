397f733e1227e4fd9fbb1e357ac72f6354ae770b
criatura435 <127697449+criatura435@users.noreply.github.com>
2023-04-22 12:52:15 -0500

    false question randomization & working assign. function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -36,27 +36,50 @@ var falseAnswers = [
     "FalseAnswer2",
     "FalseAnswer3",
     "FalseAnswer4",
+    "FalseAnswer5",
+    "FalseAnswer6",
+    "FalseAnswer7",
+    "FalseAnswer8",
+    "FalseAnswer9",
+    "FalseAnswer10",
 ]
 
-// selects question by index and displays appropriate options
+// selects question by index and displays options
 var questionGenerator = function(qnumber){
+    
     // selects and displays question by number
     question.textContent = questions[qnumber]
-    // array with correct answer & 3 random false answers to randomly assign to li's
+
+    // assigns 1 false answer to a variable and then removes that answer as an option to be selected for the next variable
+    var falseAnswer1 = falseAnswers[Math.floor(Math.random()*falseAnswers.length)]
+    var falseAnswers1 = falseAnswers
+    falseAnswers1.splice(falseAnswers1.indexOf(falseAnswer1), 1)
+    var falseAnswer2 = falseAnswers1[Math.floor(Math.random()*falseAnswers1.length)]
+    var falseAnswers2 = falseAnswers1
+    falseAnswers2.splice(falseAnswers2.indexOf(falseAnswer2), 1)
+    var falseAnswer3 = falseAnswers2[Math.floor(Math.random()*falseAnswers2.length)]
+    
+    console.log(falseAnswer1)
+    console.log(falseAnswers1)
+    console.log(falseAnswer2)
+    console.log(falseAnswers2)
+    console.log(falseAnswer3)
+
+    // array with stored answers for assignment to li's as options
     var randAnswers = [answers[qnumber],
-    falseAnswers[Math.floor(Math.random()*falseAnswers.length)],
-    falseAnswers[Math.floor(Math.random()*falseAnswers.length)],
-    falseAnswers[Math.floor(Math.random()*falseAnswers.length)]
-    ]
+    falseAnswer1, falseAnswer2, falseAnswer3]
+    
     // loop to create li's for responses and assign answers
-    for (i = 0; i < randAnswers.length; i++) {
+    for (i = 0; i < 4; i++) {
         var option = document.createElement("li")
         choices.appendChild(option)
-        // needs to assign correct answer to 1 li and 3 false answers to others (currently completely random per randAnswers)
-        option.textContent = randAnswers[Math.floor(Math.random() * 4)]
+        // assigns correct answer to 1 li and 3 false answers to others
+        var randAnswer = Math.floor(Math.random() * randAnswers.length)
+        option.textContent = randAnswers[randAnswer]
         // may not need this dependent on above solution
         option.setAttribute("id", `option${i}`)
+        randAnswers.splice(randAnswer, 1)
     }
 }
 
-questionGenerator(3)
+questionGenerator(2)
